5b8da381c85f0c34f0f0a9d0f295b271ab7bd08d
Shaun Kent <shaunkent81@gmail.com>
2021-11-04 12:21:08 -0700

    setup file to work with entity

--- Bakery/Startup.cs
+++ Bakery/Startup.cs
@@ -1,1 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Bakery.Models;
+using Microsoft.AspNetCore.Identity;
 
+namespace Bakery
+{
+  public class Startup
+  {
+    public Startup(IWebHostEnviroment env)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json");
+      Configuration = builder.Build();
+    }
+
+    public IConfigurationRoot Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<BakeryContext>(options => options
+        .UseMySql(Configuration["ConnectionString:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseRouting();
+
+      app.UseEndPoints(routes => 
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.UseStaticFiles();
+
+      app.Run(async(context) => 
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
+}
