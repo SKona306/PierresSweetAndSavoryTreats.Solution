2a522fdd1dcc72b20edc1d0de0e4b5bac5b5e3fc
Shaun Kent <shaunkent81@gmail.com>
2021-11-06 13:43:03 -0700

    add home controller and index splash page

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
