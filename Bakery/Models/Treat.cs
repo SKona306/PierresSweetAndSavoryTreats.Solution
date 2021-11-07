e2bfcb78ca6830e9c88df2bf01c3173f4099f611
Shaun Kent <shaunkent81@gmail.com>
2021-11-06 17:43:29 -0700

    add Application user to both flavor and treat class

--- Bakery/Models/Treat.cs
+++ Bakery/Models/Treat.cs
@@ -12,6 +12,7 @@ namespace Bakery.Models
     public int TreatId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public virtual ICollection<Flavor_Treat> JoinEntities { get; set; }
   }
 }
