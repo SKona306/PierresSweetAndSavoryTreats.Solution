719807d100024a60915f6b77ce35a2edb3a7f126
Shaun Kent <shaunkent81@gmail.com>
2021-11-06 18:18:45 -0700

    add list of treats and flavors to home index page

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Bakery.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly BakeryContext _db;
+    public HomeController(BakeryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      List<Treat> treatList = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
+      return View(treatList);
     }
   }
 }
